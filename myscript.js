66d25d1e8f6b8b4c44a39e99f7914974bff129f8
Dean Marano <dean.marano@gmail.com>
2020-02-18 18:29:58 -0600

    Code cleanup and publish on addons.mozilla.org.

--- myscript.js
+++ myscript.js
@@ -1,7 +1,5 @@
-console.log('myscript.js');
-    chrome.runtime.sendMessage( {
-            action: "content",
-            host: document.location.hostname,
-            content: document.body.textContent
-        }, function(response) { }
-    );
+chrome.runtime.sendMessage({
+  action: "content",
+  host: document.location.hostname,
+  content: document.body.textContent
+}, function() { });
